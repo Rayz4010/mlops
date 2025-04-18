e050b55ee074e9b123cdc9bfc72395baab96ca19
rayz <rayquaza4010@icloud.com>
2025-04-18 22:40:00 +0530

    j

--- python/basic_problem_2.py
+++ python/basic_problem_2.py
@@ -11,6 +11,8 @@ print(reverse_string("machine"))  # Output: "enihcam"
 
 
 def reverse_string(s):
+    #using string operation (though i forgot the colon part:P)
     return s[::-1]
 
+#printing the stringj
 print(reverse_string("machine"))
