86939e5cf5b164f85a4edb996816500c10484475
rayz <rayquaza4010@icloud.com>
2025-10-08 03:02:54 +0530

    modifying the front end

--- codex-projects/app.py
+++ codex-projects/app.py
@@ -6,6 +6,8 @@ import pickle as pkl
 #importing models
 iris=pkl.load(open('iris.pkl','rb'))
 
+
+#the loading function just for asthetics though
 def load():
     progress_text = "Operation in progress. Please wait."
     my_bar = st.progress(0, text=progress_text)
@@ -18,62 +20,81 @@ def load():
     #st.button("Rerun")
 
 
+
+
+
+#Sidebar features
+def sidebar():
+    global op
+    string='''Choose any of the project below\n
+            1.Iris Dataset \n
+            OR\n
+            2.House Price Prediction'''
+    st.sidebar.header("Project Selection")
+    #st.sidebar.subheader("Choose a project to explore")
+    st.sidebar.subheader(string)
+    set={"iris", "house price"}
+    op=st.sidebar.selectbox("Select a project",set )
+    if True:
+        refresh()
+    
+def space():
+    st.subheader("") 
+    
+def refresh():    
+    bt=st.sidebar.button("Refresh")
+    if bt:
+        with st.spinner('Refreshing...'):
+            time.sleep(2)
+        st.success('Refreshed!')
+
+
 #header file
 st.title("Codex Projects",width=500)
 st.subheader("",divider='blue')
 st.header("Welcome to the MLOps Codex Projects app",divider='grey')
-
+space()
 st.info("This app allows you to explore different machine learning projects.")
-#Sidebar features
-string='''Choose any of the project below\n
-          1.Iris Dataset \n
-          OR\n
-          2.House Price Prediction'''
-st.sidebar.header("Project Selection")
-#st.sidebar.subheader("Choose a project to explore")
-st.sidebar.subheader(string)
-set={"iris", "house price"}
-op=st.sidebar.selectbox("Select a project",set )
-bt=st.button("Refresh")
-if bt:
-    with st.spinner('Refreshing...'):
-        time.sleep(2)
-    st.success('Refreshed!')
+space()
 
-st.markdown("You selected the project:")
+#main body
+def main():
+    sidebar()
+    st.markdown("You selected the project:")
 
 
-if op=="iris":
-    st.write(f"You selected: {op}")
-    load()
-    petal_length=st.number_input("Enter the petal length")
-    petal_width=st.number_input("Enter the petal width")
-    sepal_length=st.number_input("Enter the sepal length")
-    sepal_width=st.number_input("Enter the sepal width")
-    data_i=[sepal_length,sepal_width,petal_length,petal_width]
-    
-    easter_egg=[69.00,69.00,69.00,69.00]
-    if data_i==easter_egg:
-        st.error("There is no such flower mate")
-        st.button("Try Again")
-    else:
-        if st.button("Predict"):
-            if petal_length and petal_width and sepal_length and sepal_width:
-                pred=iris.predict([data_i])
-                if pred==0:
-                    st.write(f"The predicted flower is Setosa")
-                elif pred==1:
-                    st.write(f"The predicted flower is Versicolor")
+    if op=="iris":
+        st.markdown(f"You selected: {op}")
+        load()
+        petal_length=st.number_input("Enter the petal length")
+        petal_width=st.number_input("Enter the petal width")
+        sepal_length=st.number_input("Enter the sepal length")
+        sepal_width=st.number_input("Enter the sepal width")
+        data_i=[sepal_length,sepal_width,petal_length,petal_width]
+        
+        easter_egg=[69.00,69.00,69.00,69.00]
+        if data_i==easter_egg:
+            st.error("There is no such flower mate")
+            st.button("Try Again")
+        else:
+            if st.button("Predict"):
+                if petal_length and petal_width and sepal_length and sepal_width:
+                    pred=iris.predict([data_i])
+                    if pred==0:
+                        st.write(f"The predicted flower is Setosa")
+                    elif pred==1:
+                        st.write(f"The predicted flower is Versicolor")
+                    else:
+                        st.write(f"The predicted flower is Virginica")
                 else:
-                    st.write(f"The predicted flower is Virginica")
-            else:
-                st.write("Please enter all the fields")
-    #st.write("Pussy is power.")
-    # Add more details or functionality for the iris-dataset project here
-else:
-    load()
-    st.write(f"You selected: {op}")
-    
-    
+                    st.write("Please enter all the fields")
+        #st.write("Pussy is power.")
+        # Add more details or functionality for the iris-dataset project here
+    else:
+        load()
+        st.write(f"You selected: {op}")
 
 
+if __name__ == '__main__':
+    main()
+    pass
